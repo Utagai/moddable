8d50f9b6e012b40f833e0ac9625807bd1b587d52
may h <mhoque95@gmail.com>
2026-05-30 18:06:22 -0400

    more newlines

--- build/devices/pebble/modules/httpclient/httpclient-pebble.js
+++ build/devices/pebble/modules/httpclient/httpclient-pebble.js
@@ -283,7 +283,7 @@ class HTTPClient {
 					}
 					current.statusText = message.get(BASE + 11);
 					trace(`[httpclient] receiveStatus got status=${current.status} statusText=${JSON.stringify(current.statusText)}\n`);
-					trace(`[httpclient] receiveHeaders message index values: ${message.get(BASE+7)}`);
+					trace(`[httpclient] receiveHeaders message index values: ${message.get(BASE+7)}\n\n`);
 					break;
 
 					case "receiveHeaders": {
@@ -296,9 +296,9 @@ class HTTPClient {
 						let headers = current.headers.join("");
 						delete current.headers;
 						const map = new Map;
-						trace(`[httpclient] receiveHeaders got headers:\n${JSON.stringify(headers)}\n`);
-						trace(`[httpclient] receiveStatus message index values: status=${message.get(BASE + 6)}, text=${message.get(BASE+11)}\n`);
-						trace(`[httpclient] receiveBody message index values: ${message.get(BASE + 8)}\n`);
+						trace(`[httpclient] receiveHeaders got headers:\n${JSON.stringify(headers)}\n\n`);
+						trace(`[httpclient] receiveStatus message index values: status=${message.get(BASE + 6)}, text=${message.get(BASE+11)}\n\n`);
+						trace(`[httpclient] receiveBody message index values: ${message.get(BASE + 8)}\n\n`);
 						headers.split("\n").forEach(header => {
 							if (!header) return;
 							const [key, value] = header.split(":");
